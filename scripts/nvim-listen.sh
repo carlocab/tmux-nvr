f22adb0843d0b236d7f0fdaa23131cc9323e43db
carlocab <30379873+carlocab@users.noreply.github.com>
2020-11-03 03:51:36 +0000

    Rename socket directory
    The new name is also the name of the plugin.

--- scripts/nvim-listen.sh
+++ scripts/nvim-listen.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 temp="$(dirname "$(mktemp -u)")"
-socketdir="${temp}/nvr-tmux"
+socketdir="${temp}/tmux-nvr"
 [ -d "$socketdir" ] || mkdir -p "$socketdir"
 tmux set-environment NVIM_LISTEN_ADDRESS "$(mktemp -u "${socketdir}/XXXXXX")"
