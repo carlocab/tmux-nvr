12c635781cbef069aaa62dc088e338251369f7d2
carlocab <30379873+carlocab@users.noreply.github.com>
2020-11-03 17:03:37 +0000

    Add documentation for `nvim-listen.sh`

--- scripts/nvim-listen.sh
+++ scripts/nvim-listen.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# Creates an unused socket name in $TMPDIR/tmux-nvr
+# Note that we don't want the `-g` flag for `set-environment`, since we want
+# NVIM_LISTEN_ADDRESS to be set for each session.
 
 temp="$(dirname "$(mktemp -u)")"
 socketdir="${temp}/tmux-nvr"
