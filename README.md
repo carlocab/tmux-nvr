60546fc8e32a10ba3e94ff0e8b4ff0d41a3323cb
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-06-01 19:09:40 +0800

    Improve Zsh plugin
    1. Bail out earlier if `tmux` is not executable.
    2. Silence errors when `NVIM_LISTEN_ADDRESS` is not set in the tmux
       environment.

--- README.md
+++ README.md
@@ -37,7 +37,7 @@ Next, add this snippet to your `zshrc` or `bashrc`.
 
 ```bash
 if [ -n "$TMUX" ]; then
-    eval "$(tmux show-environment -s NVIM_LISTEN_ADDRESS)"
+    eval "$(tmux show-environment -s NVIM_LISTEN_ADDRESS 2> /dev/null)"
 else
     export NVIM_LISTEN_ADDRESS=/tmp/nvimsocket
 fi
--- tmux-nvr.plugin.zsh
+++ tmux-nvr.plugin.zsh
@@ -1,22 +1,22 @@
 # tmux-nvr.plugin.zsh
 # Sets NVIM_LISTEN_ADDRESS and adds `nvr-tmux` to path
 
+# Bail out early if `tmux` is not executable
+(( $+commands[tmux] )) || return 0
+
 # Standardised $0 handling.
 # See https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc#1-standardized-0-handling
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-# If in tmux, set `NVIM_LISTEN_ADDRESS` using tmux-nvr
 if [[ -n "$TMUX" ]]; then
-    eval "$(command tmux show-environment -s NVIM_LISTEN_ADDRESS)"
+    # Silence errors when `NVIM_LISTEN_ADDRESS` is not set in the tmux environment.
+    eval "$(command tmux show-environment -s NVIM_LISTEN_ADDRESS 2> /dev/null)"
 else
     # -m 700 sets permissions so that only you have access to this directory
     mkdir -p -m 700 /tmp/nvr
     export NVIM_LISTEN_ADDRESS=/tmp/nvr/nvimsocket
 fi
-
-# Bail out early if `tmux` is not executable
-(( $+commands[tmux] )) || return 0
 
 # Strip all but the digits from `tmux -V`
 tmux_version_digits="${$(command tmux -V 2> /dev/null)//[^0-9]/}"
